1097d1bc6393866c5f9dabd1f832e291432a4b05
Zhaomeng Zhu <zhaomeng.zhu@gmail.com>
2018-02-02 15:57:31 +0800

    add polybar and termite

--- init.sh
+++ init.sh
@@ -5,7 +5,9 @@ ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/yaourtrc ~/.yaourtrc
 ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/config/i3 ~/.config/i3
+ln -sf ~/.dotfiles/config/termite ~/.config/termite
 ln -sf ~/.dotfiles/config/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sf ~/.dotfiles/config/polybar/config ~/.config/polybar/config
 ln -sf ~/.dotfiles/config/compton.conf ~/.config/compton.conf
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/pam_environment ~/.pam_environment
